58afd2f5966543f7ec8212f82217e25e86296b51
GeNeRaL <abood.v000@email.com>
2025-05-03 00:06:06 +0300

    Starting

---
+++ Portfolio/src/Introduce.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Introduce() {
+  return (
+    <>
+      <div
+        style={{
+          background: "orange",
+          height: "80vh",
+          display: "flex",
+          justifyContent: "space-around",
+        }}
+      >
+        <div>
+          <h1>اهلا</h1>
+        </div>
+        <div>dd</div>
+      </div>
+    </>
+  );
+}
