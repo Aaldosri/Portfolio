804e3859b557ae10688a5a38a2fae195e59d409f
Aaldosri <a.aldosri.sa@gmail.com>
2025-10-17 16:57:48 +0300

    Finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,7 @@
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      screens: {
-        xs: { max: "500px" }, // شاشات صغيرة جداً
-        sm: "640px", // شاشات صغيرة
-        md: "768px", // شاشات متوسطة
-        lg: "1024px", // شاشات كبيرة
-        xl: "1280px", // شاشات أكبر
-        "2xl": "1536px", // شاشات فائقة الحجم
-      },
-    },
+    extend: {},
   },
   darkMode: "class",
   plugins: [],
